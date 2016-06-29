0ba080a4d01fdd8a5109347bdc33871df21a5bd2
sebastianseidelmann <sebastian.seidelmann@wfp2.com>
2016-06-29 16:04:17 +0200

    [TASK] Add the semver exception

---
+++ src/Seidelmann/DevUtils/Exception/VersionNotSemanticVersioningException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class VersionNotSemanticVersioningException
+ * @package Seidelmann\DevUtils\Exception
+ * @author Sebastian Seidelmann <sebastian.seidelmann@wfp2.com>, wfp:2 GmbH & Co. KG
+ */
+
+namespace Seidelmann\DevUtils\Exception;
+
+/**
+ * Class VersionNotSemanticVersioningException
+ * @package Seidelmann\DevUtils\Exception
+ * @author Sebastian Seidelmann <sebastian.seidelmann@wfp2.com>, wfp:2 GmbH & Co. KG
+ */
+class VersionNotSemanticVersioningException extends DevutilsException
+{
+
+}
