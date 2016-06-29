866412634d816fc381b72b436585878dd697f84b
sebastianseidelmann <sebastian.seidelmann@wfp2.com>
2016-06-29 15:51:26 +0200

    [TASK] Add the base exception

---
+++ src/Seidelmann/DevUtils/Exception/DevutilsException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class DevutilsException
+ * @package Seidelmann\DevUtils\Exception
+ * @author Sebastian Seidelmann <sebastian.seidelmann@wfp2.com>, wfp:2 GmbH & Co. KG
+ */
+
+namespace Seidelmann\DevUtils\Exception;
+
+/**
+ * Class DevutilsException
+ * @package Seidelmann\DevUtils\Exception
+ * @author Sebastian Seidelmann <sebastian.seidelmann@wfp2.com>, wfp:2 GmbH & Co. KG
+ */
+class DevutilsException extends \Exception
+{
+
+}
